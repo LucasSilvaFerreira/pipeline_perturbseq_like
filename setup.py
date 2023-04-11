7937b4381b29fde374ac605792491b584159d927
LucasSilvaFerreira <lf114@compute-a-16-48.o2.rc.hms.harvard.edu>
2023-04-11 17:27:30 -0400

    removing update

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="GTFProcessing",
-    version="0.0.1",
-    py_modules=["GTFProcessing", "guide_table_processing", "PerturbLoader_generation", "preprocessing", "runSceptre", "test_multiple_files"],
-    install_requires=["pandas", "gtfparse", "tqdm"]
-)
